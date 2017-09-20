e7cc41981aafe8cf5b9b541a8336169f6d1e91ed
Christian Duvholt <christian@duvholt.net>
2017-09-20 10:37:01 +0200

    Add __str__ to models

--- backend/noticenter/api/models.py
+++ backend/noticenter/api/models.py
@@ -12,8 +12,15 @@ class Server(models.Model):
             return latest_ping.ip
         return None
 
+    def __str__(self):
+        return self.name
+
 
 class Ping(models.Model):
     server = models.ForeignKey(Server, related_name='pings', on_delete=models.CASCADE)
     ip = models.CharField(max_length=50)
     time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        timestamp = self.time.strftime('%Y-%m-%d %H:%M:%S')
+        return f'Ping from {self.server}({self.ip}) at {timestamp}'
