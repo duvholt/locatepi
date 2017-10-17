67a2071366fc72b9ffafeb49f8a6c11913dc8019
Christian Duvholt <christian@duvholt.net>
2017-10-17 22:55:23 +0200

    Add support to show difference between offline/online

--- frontend/src/components/Server/index.js
+++ frontend/src/components/Server/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import css from './Server.css';
 
-const Server = ({ name, ip, lastUpdate }) => (
+const Server = ({ name, ip, offline, lastUpdate }) => (
   <div className={css.component}>
-    <div className={css.name}>{ name }</div>
+    <div
+      className={classNames(
+        css.name, {
+          [css.offline]: offline,
+        }
+      )}
+    >
+      { name }
+    </div>
     <div className={css.content}>
       <div className={css.ip}>{ ip }</div>
       <div className={css.date}>Last update: 2017-10-10 13:44</div>
@@ -12,10 +21,15 @@ const Server = ({ name, ip, lastUpdate }) => (
   </div>
 );
 
+Server.defaultProps = {
+  offline: false,
+}
+
 Server.propTypes = {
   name: PropTypes.string.isRequired,
   ip: PropTypes.string.isRequired,
   lastUpdate: PropTypes.instanceOf(Date).isRequired,
+  offline: PropTypes.bool,
 }
 
 export default Server;
